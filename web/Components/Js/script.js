4c5fd2989cd02af9c54778878fd4d3e5cd2450df
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2023-10-05 14:05:09 +0800

    up

---
+++ web/Components/Js/script.js
@@ -0,0 +1,442 @@
+// variable declaration
+let peer;
+let roomId;
+const remoteVideo = document.getElementById("remote-video");
+const rect = remoteVideo.getBoundingClientRect();
+const width = rect.width > rect.height ? rect.width : rect.height;
+const height = rect.width > rect.height ? rect.height : rect.widt;
+let streamSetting = {
+  video: {
+    frameRate: { ideal: 12, max: 20 },
+    width: screen.height,
+    height: screen.width,
+  },
+  audio: {
+    echoCancellation: true,
+    noiseSuppression: true,
+  },
+};
+let localStream;
+let selfStream;
+let remoteStreamTest;
+let currentPeer;
+let dataConnection;
+
+// Promise for using Media devices
+var getUserMedia =
+  navigator.mediaDevices.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+
+// @@@@@@@@@@@@@@
+// onClick functionality for Create Room Button
+document.getElementById("create-room").addEventListener("click", function () {
+  if (!getUserMedia) {
+    alert("Your browser is not supported! Please update browsers.");
+    return;
+  }
+
+  document
+    .getElementById("create-room")
+    .children[1].classList.add("fas", "fa-circle-notch", "fa-spin");
+  roomId = randomId(3) + "-" + randomId(3);
+
+  // create peer with id
+  peer = new Peer(roomId);
+
+  // Open event
+  peer.on("open", function (id) {
+    if (getUserMedia) {
+      console.log("In peer open event");
+      getUserMedia(streamSetting)
+        .then(function (stream) {
+          localStream = stream;
+          localVideo();
+          document.getElementById("meeting-id").value = roomId;
+          createCanvas();
+          document
+            .getElementById("create-room")
+            .children[1].classList.remove("fas", "fa-circle-notch", "fa-spin");
+        })
+        .catch(function (error) {
+          alert("Failed to create room");
+          setTimeout(function () {
+            window.location.reload();
+          }, 2000);
+        });
+    } else {
+      alert("Your browser is not supported! Please update browsers.");
+      return;
+    }
+
+    console.log("Out of open event");
+  });
+
+  // Call event
+  peer.on("call", function (data) {
+    document.getElementById("show-msg").style.display = "none";
+    data.answer(localStream);
+    data.on("stream", function (remoteStream) {
+      var video = document.querySelector("#remote-video");
+      remoteStreamTest = remoteStream;
+      currentPeer = data.peerConnection;
+      video.srcObject = remoteStream;
+      video.play();
+    });
+  });
+
+  // Connection event
+  peer.on("connection", function (data) {
+    console.log("Connected with Host");
+    dataConnection = data;
+
+    data.on("data", function (data) {
+      console.log("server: " + data);
+      let message = '<p class="remote-msg"> <span>' + data + "</span></p>";
+      document.getElementById("chat").innerHTML += message;
+      scrollBottom();
+    });
+  });
+
+  // On disconnect
+  peer.on("disconnected", function () {
+    // Reload the window when disconnected
+    setTimeout(function () {
+      peer.destroy();
+      window.location.reload();
+    }, 500);
+    // peer.destroy();
+    // window.location.reload();
+  });
+});
+
+/*
+@@@@@@@@@@@@@@
+ Function to join with roomId
+*/
+
+document.getElementById("join-room").addEventListener("click", function () {
+  var roomId = document.getElementById("room-id").value;
+  if (roomId == "" || roomId == " ") {
+    alert("please enter room no");
+    return;
+  }
+  roomId = roomId.toLowerCase();
+  if (!getUserMedia) {
+    alert("Your browser is not supported ! Please use updated browsers.");
+    return;
+  }
+
+  document
+    .getElementById("join-room")
+    .children[0].classList.add("fas", "fa-circle-notch", "fa-spin");
+
+  peer = new Peer();
+
+  // open event
+  peer.on("open", function (id) {
+    dataConnection = peer.connect(roomId);
+    dataConnection.on("data", function (data) {
+      let message = '<p class="remote-msg"><span>' + data + "</span></p>";
+      document.getElementById("chat").innerHTML += message;
+      scrollBottom();
+    });
+
+    if (getUserMedia) {
+      //getUserMedia function call
+      getUserMedia(streamSetting)
+        .then(function (stream) {
+          localStream = stream;
+          localVideo();
+          document.getElementById("show-msg").innerHTML = "Connecting ....";
+          createCanvas();
+          document
+            .getElementById("join-room")
+            .children[0].classList.remove("fas", "fa-circle-notch", "fa-spin");
+
+          let call = peer.call(roomId, localStream);
+          call.on("stream", function (stream) {
+            document.getElementById("show-msg").innerHTML = "";
+            currentPeer = call.peerConnection;
+            var video = document.querySelector("#remote-video");
+            video.srcObject = stream;
+            video.play();
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      console.log("getUserMedia not supported ! Please use updated browsers.");
+    }
+  });
+
+  // disconnected event
+  peer.on("disconnected", function () {
+    peer.destroy();
+    window.location.reload();
+  });
+
+  // // close event
+  peer.on("close", function () {
+    peer.destroy();
+    window.location.reload();
+  });
+});
+
+/*
+  SCREEN SHARE OPTION
+*/
+
+let screenShare = false;
+let videoOnScreen;
+
+document.getElementById("share-screen").addEventListener("click", function () {
+  if (!screenShare) {
+    screenShare = true;
+    document
+      .getElementById("share-screen")
+      .children[0].classList.remove("fas", "fa-desktop");
+    document
+      .getElementById("share-screen")
+      .children[0].classList.add("fas", "fa-times");
+
+    navigator.mediaDevices
+      .getDisplayMedia({
+        video: {
+          cursor: "always",
+        },
+        audio: true,
+      })
+      .then((stream) => {
+        videoOnScreen = stream.getVideoTracks()[0];
+        videoOnScreen.onended = function () {
+          stopScreenShare();
+        };
+        let host = currentPeer.getSenders().find((e) => {
+          return e.track.kind == videoOnScreen.kind;
+        });
+        host.replaceTrack(videoOnScreen);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    screenShare = false;
+    stopScreenShare();
+    document
+      .getElementById("share-screen")
+      .children[0].classList.remove("fas", "fa-times");
+    document
+      .getElementById("share-screen")
+      .children[0].classList.add("fas", "fa-desktop");
+  }
+});
+
+/*
+  Chat box functions
+*/
+
+// enabling/disabling chat container
+document.getElementById("chat-container").style.display = "none";
+document.getElementById("open-chat").addEventListener("click", (e) => {
+  let element = document.getElementById("chat-container");
+  if (element.style.display == "none") {
+    element.style.display = "block";
+    document
+      .getElementById("open-chat")
+      .children[0].classList.remove("fas", "fa-comment-alt");
+    document
+      .getElementById("open-chat")
+      .children[0].classList.add("fas", "fa-times");
+  } else {
+    element.style.display = "none";
+    document
+      .getElementById("open-chat")
+      .children[0].classList.remove("fas", "fa-times");
+    document
+      .getElementById("open-chat")
+      .children[0].classList.add("fas", "fa-comment-alt");
+  }
+});
+
+//send message function
+document.getElementById("send-msg").addEventListener("click", function () {
+  let message = document.getElementById("msg").value;
+  if (dataConnection == undefined) {
+    alert(
+      "No one else is in this meeting! You can send message when someone joins the meeting."
+    );
+    return;
+  }
+  dataConnection.send(message);
+  console.log("Appending sender msg");
+  let appendMsg = '<p class="self-msg"><span>' + message + `</span></p>`;
+  document.getElementById("chat").innerHTML += appendMsg;
+  scrollBottom();
+  document.getElementById("msg").value = "";
+});
+
+/* 
+    UTILITY FUNCTIONS
+    ###
+    ####
+    #####
+    ######
+*/
+// To set localVideo
+function localVideo() {
+  if (getUserMedia) {
+    getUserMedia({
+      video: {
+        width: 720,
+        height: 480,
+      },
+      audio: false,
+    })
+      .then(function (stream) {
+        selfStream = stream;
+        var video = document.querySelector(`#self-video`);
+        video.srcObject = selfStream;
+        video.onloadedmetadata = function (e) {
+          video.play();
+        };
+      })
+      .catch(function (error) {
+        // If setting the video stream fails, show an alert
+        alert("Failed to set Video");
+
+        // Reload the window after a delay (2 seconds in this case)
+        setTimeout(function () {
+          window.location.reload();
+        }, 2000);
+      });
+  } else {
+    alert("Your browser is not supported! Please update browsers.");
+    return;
+  }
+}
+// Random Id creater
+function randomId(length) {
+  var result = [];
+  var chars = "abcdefghijklmnopqrstuvwxy";
+  var charsLength = chars.length;
+
+  for (let i = 0; i < length; i++) {
+    result.push(chars.charAt(Math.floor(Math.random() * charsLength)));
+  }
+
+  return result.join("");
+}
+
+// Canvas Creation & Deletion
+function createCanvas() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("meeting").style.display = "block";
+}
+
+function destroyCanvas() {
+  document.getElementById("meeting").style.display = "none";
+  document.getElementById("home").style.display = "block";
+}
+
+// Chat scroll function
+function scrollBottom() {
+  let element = document.getElementById("chat");
+  if (element.scrollTop + element.clientHeight != element.scrollHeight) {
+    element.scrollTop = element.scrollHeight;
+  }
+}
+
+/*
+    ACTION BUTTONS FUNCTIONALITY
+*/
+
+// CopyToClipboard
+document
+  .querySelector(".copy-meeting-id")
+  .children[1].addEventListener("click", function () {
+    let text = document.getElementById("meeting-id");
+    text.select();
+    text.setSelectionRange(0, 999999);
+    document.execCommand("copy");
+    document.getElementById("coppied-msg").innerText = "Meeting id coppied!";
+    setTimeout(function () {
+      document.getElementById("coppied-msg").innerText = "";
+    }, 3000);
+  });
+
+// Mute audio
+document.getElementById("mute-audio").addEventListener("click", function () {
+  if (localStream.getAudioTracks()[0]["enabled"]) {
+    localStream.getAudioTracks()[0]["enabled"] = false;
+    let element = document.getElementById("mute-audio").children[0];
+    element.classList.remove("fa-microphone-alt");
+    element.classList.add("fa-microphone-alt-slash");
+  } else {
+    let element = document.getElementById("mute-audio").children[0];
+    localStream.getAudioTracks()[0]["enabled"] = true;
+    element.classList.remove("fa-microphone-alt-slash");
+    element.classList.add("fa-microphone-alt");
+  }
+});
+
+// Mute video
+document.getElementById("mute-video").addEventListener("click", function () {
+  if (localStream.getVideoTracks()[0]["enabled"]) {
+    localStream.getVideoTracks()[0]["enabled"] = false;
+    let element = document.getElementById("mute-video").children[0];
+    element.classList.remove("fa-video");
+    element.classList.add("fa-video-slash");
+  } else {
+    localStream.getVideoTracks()[0]["enabled"] = true;
+    let element = document.getElementById("mute-video").children[0];
+    element.classList.remove("fa-video-slash");
+    element.classList.add("fa-video");
+  }
+  selfStream.getVideoTracks()[0]["enabled"] =
+    !selfStream.getVideoTracks()[0]["enabled"];
+});
+
+// End Call
+document.getElementById("call-end").addEventListener("click", function () {
+  if (confirm("Are you sure to end the meet?")) {
+    localStream.getTracks().forEach((track) => {
+      track.enabled = false;
+      track.stop();
+    });
+    selfStream.getTracks().forEach((track) => {
+      track.enabled = false;
+      track.stop();
+    });
+    destroyCanvas();
+    peer.disconnect();
+    peer.close();
+  }
+});
+
+// STOP SCREEN SHARE
+function stopScreenShare() {
+  let videoTrack = localStream.getVideoTracks()[0];
+  let host = currentPeer.getSenders().find((e) => {
+    return e.track.kind == videoTrack.kind;
+  });
+  host.replaceTrack(videoTrack);
+}
+
+/*
+  Handling textbox
+*/
+
+document.getElementById("room-id").addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("join-room").click();
+  }
+});
+
+document.getElementById("msg").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    document.getElementById("send-msg").click();
+  }
+});
